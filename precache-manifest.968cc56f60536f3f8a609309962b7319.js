3b25a041cfd45d2901e8e67b7596769290fcf6be
Sulaymon Olimov <sulaymonlucky@gmail.com>
2020-07-02 17:59:01 +0500

    Updates

---
+++ precache-manifest.968cc56f60536f3f8a609309962b7319.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcb58f6cea42a7a345a3117231666e2",
+    "url": "/vertka-bikee/index.html"
+  },
+  {
+    "revision": "8985e4881bf773076c06",
+    "url": "/vertka-bikee/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bd99f4277d65618bbbfa",
+    "url": "/vertka-bikee/static/js/2.1baadb23.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vertka-bikee/static/js/2.1baadb23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8985e4881bf773076c06",
+    "url": "/vertka-bikee/static/js/main.1c6ac17f.chunk.js"
+  },
+  {
+    "revision": "f1f47936e30ea29061e0",
+    "url": "/vertka-bikee/static/js/runtime-main.65f04f05.js"
+  },
+  {
+    "revision": "245d514eaf332641bdec40d6023219ed",
+    "url": "/vertka-bikee/static/media/boyBike.245d514e.png"
+  },
+  {
+    "revision": "91f33c563ec4dabb6b388fd9e30db668",
+    "url": "/vertka-bikee/static/media/girlBike.91f33c56.png"
+  }
+]);
